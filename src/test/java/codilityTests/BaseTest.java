9fadc4012e82f1cefc2af40034681f4e25e15a2f
dasaevbrune <dasaev.brune@gmail.com>
2024-02-07 19:37:55 -0500

    First commit Add TCs

---
+++ src/test/java/codilityTests/BaseTest.java
@@ -0,0 +1,26 @@
+package codilityTests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://codility-frontend-prod.s3.amazonaws.com/media/task_static/qa_login_page/9a83bda125cd7398a3d6d45ea4/static/attachments/reference_page.html");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/codilityTests/LoginPageTest.java
@@ -0,0 +1,106 @@
+package codilityTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends BaseTest {
+
+    //locators
+    //e-mail = //input[@id='email-input']
+    //password = //input[@id='password-input']
+    //login button = //input[@id='password-input']
+    //success msg = //div[@Class='message success']
+    //error msg = //div[@Class='message error']
+    //email validation msg = /div[@class='validation error' and contains(text(), 'Enter a valid email')]
+    //password required msg =//div[@class='validation error' and contains(text(), 'Password is required')]
+    //email required msg = /div[@class='validation error' and contains(text(), 'Email is required')]
+
+
+
+    @Test
+    public void verifyEmailPasswFields() {
+        WebElement emailField = driver.findElement(By.id("email-input"));
+        WebElement passwordField = driver.findElement(By.id("password-input"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        Assert.assertTrue(emailField.isDisplayed(), "Email field is displayed");
+        Assert.assertTrue(passwordField.isDisplayed(), "Password field is displayed");
+        Assert.assertTrue(loginBtn.isDisplayed(), "Login button is present");
+        /* if(emailField.isDisplayed()){
+             System.out.println("OK");
+         }*/
+
+    }
+
+
+    @Test
+    public void verifyLoginSuccess() {
+        WebElement emailField = driver.findElement(By.id("email-input"));
+        WebElement passwordField = driver.findElement(By.id("password-input"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        emailField.sendKeys("login@codility");
+        passwordField.sendKeys("password");
+        loginBtn.click();
+
+        WebElement successMsg = driver.findElement(By.xpath("//div[@Class='message success']"));
+        Assert.assertTrue(successMsg.isDisplayed(), "Succes Message is displayed");
+        System.out.println(successMsg.isDisplayed());
+
+    }
+
+
+    @Test
+    public void verifyIvalidCredentials() {
+        WebElement emailField = driver.findElement(By.id("email-input"));
+        WebElement passwordField = driver.findElement(By.id("password-input"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        emailField.sendKeys("unknown@codility");
+        passwordField.sendKeys("password");
+        loginBtn.click();
+
+        WebElement invalidCredentialMsg = driver.findElement(By.xpath("//div[@Class='message success']"));
+
+        Assert.assertTrue(invalidCredentialMsg.isDisplayed(), "You shall not pass! Arr! Message is displayed");
+        System.out.println(invalidCredentialMsg.isDisplayed());
+    }
+
+    @Test
+    public void verifyEmailValidation() {
+        WebElement emailField = driver.findElement(By.id("email-input"));
+        WebElement passwordField = driver.findElement(By.id("password-input"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        emailField.sendKeys("dasa@dsdsd.com");
+        passwordField.sendKeys("password");
+        loginBtn.click();
+
+        WebElement invalidEmailMsg = driver.findElement(By.xpath("/div[@class='validation error' and contains(text(), 'Enter a valid email')]"));
+
+        Assert.assertTrue(invalidEmailMsg.isDisplayed(), "Enter a valid email Message is displayed");
+        System.out.println(invalidEmailMsg.isDisplayed());
+    }
+
+    @Test
+    public void verifyEmptyCredential() {
+        WebElement emailField = driver.findElement(By.id("email-input"));
+        WebElement passwordField = driver.findElement(By.id("password-input"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        emailField.sendKeys(" ");
+        passwordField.sendKeys(" ");
+        loginBtn.click();
+
+        WebElement emailReqMsg = driver.findElement(By.xpath("/div[@class='validation error' and contains(text(), 'Email is required')]"));
+        WebElement pswReqMsg = driver.findElement(By.xpath("//div[@class='validation error' and contains(text(), 'Password is required')]"));
+
+        Assert.assertTrue(emailReqMsg.isDisplayed(), "Email is required Message is displayed");
+        Assert.assertTrue(pswReqMsg.isDisplayed(), "Password is required Message is displayed");
+
+
+    }
+}
